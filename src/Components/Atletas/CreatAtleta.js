9269971c17d08f0a8d4ffa58e61ea41f70b593bb
pierovillata <ironpiero1@gmail.com>
2020-12-10 21:33:38 -0300

    Creado formulario Atleta

--- src/Components/Atletas/CreatAtleta.js
+++ src/Components/Atletas/CreatAtleta.js
@@ -1,50 +1,64 @@
-import "./Atleta.css";
-import React from "react";
+import React, { Component } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  CardFooter,
+} from "reactstrap";
+import Crud from "../Commons/Crud";
 
-export default function CreatAtleta() { 
+export default class CreatAtleta extends Component {
+
+  calcularEdad(){
     
-    function calcularEdad(event) {
-        //let fechaNac = document.querySelector("edad");
-        let fechaNac=event.target.value;
-        if(fechaNac!=='' && fechaNac!=null){
-            alert(fechaNac);
-        }
-    }
-  
-  return (
-    <div className="contentAtleta">
-      <form id="createAtleta" className="formAtleta">
-        <label>Nombres</label>
-        <input type="text" id="apellidos"></input>
-        <label>Apellidos</label>
-        <input type="text" id="nombres"></input>
-        <hr></hr>
-        <label>Fecha de Nac</label>
-        <input
-          id="fechanac"
-          type="date"
-          date-format="yyyy-mm-dd"
-          onChange={calcularEdad}
-        ></input>
-        <label>Edad</label>
-        <input className="edadAtleta" type="number"></input>
-        <hr></hr>
-        <label>Nivel</label>
-        <select id="nivel">
-          <option value="principiante">Principiante</option>
-          <option value="intermedio">Intermedio</option>
-          <option value="avanzado">Avanzado</option>
-        </select>
-        <label>Categoria</label>
-        <select id="categoria">
-          <option value="infantes">Infantes</option>
-          <option value="cadetes">Cadetes</option>
-          <option value="juniors">Juniors</option>
-          <option value="sub23">Sub23</option>
-        </select>
-      </form>
-    </div>
-  );
-  
-
+    var fecha= document.querySelector("fecha");
+   console.log(fecha)
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col xs-12>
+            <Card>
+              <CardHeader>
+                <h3>Nuevo Atleta</h3>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                  <FormGroup>
+                    <Label for="nombres">Nombres:</Label>
+                    <Input type="text" id="nombres"></Input>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="apellidos">Apellidos: </Label>
+                    <Input type="text" id="apellidos"></Input>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="fechanac">Fecha Nacimiento: </Label>
+                    <Input type="date" id="fechanac" onChange="calcularEdad"></Input>
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for="edad">Edad</Label>
+                    <Input type="number" id="nombre" min="1" max="100"></Input>
+                  </FormGroup>
+                </Form>
+              </CardBody>
+              <CardFooter>
+                <center>
+                  <Crud></Crud>
+                </center>
+              </CardFooter>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
--- src/Components/Atletas/ListAtletas.js
+++ src/Components/Atletas/ListAtletas.js
@@ -1,20 +1,8 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import CreatAtleta from "./CreatAtleta";
 
 export default class ListAtletas extends Component {
   render() {
-    return (
-      <Fragment>
-        <div>
-          <p>Listado de atletas</p>
-          <ul>
-              <li>Atleta 1</li>
-          </ul>
-        </div>
-        <div>
-          <CreatAtleta></CreatAtleta>
-        </div>
-      </Fragment>
-    );
+    return <CreatAtleta></CreatAtleta>
   }
 }
